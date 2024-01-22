22b5200e2baf77e4b47af2751a36322726ed23f2
admin <admin@admins-MacBook-Pro-6.local>
2024-01-22 15:08:09 +0900

    feat : step3

--- src/test/java/next/reflection/Junit4Runner.java
+++ src/test/java/next/reflection/Junit4Runner.java
@@ -2,11 +2,24 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
 public class Junit4Runner {
     @Test
     public void run() throws Exception {
         Class clazz = Junit4Test.class;
+        Object instance = clazz.getDeclaredConstructor().newInstance();
 
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .forEach(method -> {
+                    try {
+                        method.invoke(instance);
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 }
 
